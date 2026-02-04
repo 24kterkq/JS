4ee90e25b5ad9675cda4dc7e08285caa4104882e
24kterkq <terezka.hub@gmail.com>
2026-02-04 11:36:04 +0100

    hodina inf

---
+++ 3.3.2/script.js
@@ -0,0 +1,21 @@
+let userName = "Tereza";
+console.log(userName) 
+
+userName = "Honzík";
+console.log(userName)
+
+console.log(7+4);
+console.log("7"+"4");
+console.log("A"+"B");
+console.log("7"+4);
+
+let age = 31;
+console.log(age > 20);
+
+let text = "Text"
+console.log(typeof text)
+
+let a;
+console.log(a);
+let b = null;
+console.log(b)
