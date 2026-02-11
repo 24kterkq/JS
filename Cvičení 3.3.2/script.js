86f99ff8059be0baa692ad85fd2fdef09ed1b98c
24kterkq <terezka.hub@gmail.com>
2026-02-11 11:43:41 +0100

    update

---
+++ Cvičení 3.3.2/script.js
@@ -0,0 +1,13 @@
+let userName = "Alice";
+console.log(userName);
+
+let username = "Bob";
+console.log(username);
+
+let age1 = 21;
+let age2 = 21;
+
+console.log(age1 + age2);
+
+let text = "Combined age of Alice and Bob is 42";
+console.log("Combined age of Alice and Bob is 42");
