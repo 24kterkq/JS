8cbf1c17a530663b831adeb547f2ed7d3e4fa87d
24kterkq <terezka.hub@gmail.com>
2026-04-29 11:16:33 +0200

    update

---
+++ 3.3.2 cvičení/script.js
@@ -0,0 +1,15 @@
+let username = "Terka";
+console.log(username)
+
+username = "TerýsekVydrýsek"
+console.log(username)
+
+let age1 = 17
+console.log(age1)
+let age2 = 8
+console.log(age2)
+
+console.log(age1 + age2)
+
+let text = "chci domů"
+console.log(text)
