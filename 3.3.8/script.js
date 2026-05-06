b7f499436c9d0a5cf88aeb3ad30a57aa8ac7e8c8
24kterkq <terezka.hub@gmail.com>
2026-05-06 11:17:58 +0200

    Update script.js

---
+++ 3.3.8/script.js
@@ -0,0 +1,33 @@
+function dejMi1() {
+    return 1;
+}
+
+console.log(dejMi1())
+
+// funkce s logem
+function nasobic(a,b) {
+    console.log(a*b);
+}
+
+let r = nasobic(5,3)
+console.log(r)
+
+//Funkce s return
+
+function nasobic2(a,b) {
+    return a * b;
+}
+
+let r2 = nasobic2(5,3)
+console.log(r2 + 1)
+
+// return ukončí funkci
+
+function konec() {
+    console.log("A");
+    return "Hotovo";
+    console.log("B"); // toto neprojde
+}
+
+let konec2 = konec();
+console.log(konec());
