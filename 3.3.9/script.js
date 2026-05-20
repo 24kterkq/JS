84e9a9f824237d2d6108862704e84a5eb58d365e
24kterkq <terezka.hub@gmail.com>
2026-05-20 11:31:35 +0200

    update

---
+++ 3.3.9/script.js
@@ -0,0 +1,22 @@
+console.log("It's a nice day")
+console.log('Moje jméno je "Tereza"')
+
+let username = "Terka"
+let message = `Moje jméno je ${username}`;
+console.log(message);
+
+let a = 5;
+let b = 10;
+console.log(`Součet je ${a + b}`);
+
+function pozdrav(name) {
+    return`Hello, ${name}!`;
+}
+
+console.log(`${pozdrav('Terka')}`);
+
+let popis = `Toto je delší text,
+který se rozprostírá
+přes několik řádků.`
+
+console.log(popis)
